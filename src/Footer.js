b0fd3b943ca2e2dc155a1139444478dab428c0ac
Peter Maldonado <peterhmaldonado@gmail.com>
2022-07-14 12:35:22 -0400

    Updates footer and description.

--- src/Footer.js
+++ src/Footer.js
@@ -9,14 +9,14 @@ function Footer() {
                 <a href="https://github.com/pmaldonado" className="icon" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={['fab', 'github']} />
                 </a>
-                <a href="https://twitter.com/phmaldonado__" className="icon" target="_blank" rel="noreferrer">
+                <a href="https://twitter.com/pmaldonado__" className="icon" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={['fab', 'twitter']} />
                 </a>
                 <a href="https://www.linkedin.com/in/peterhmaldonado/" className="icon" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={['fab', 'linkedin']} />
                 </a>
             </div>
-            <p>Made with <FontAwesomeIcon icon={['fas', 'heart']} id="Footer-heart" /> in California</p>
+            {/* <p>Made with <FontAwesomeIcon icon={['fas', 'heart']} id="Footer-heart" /> in California</p> */}
         </footer>
     );
 }
